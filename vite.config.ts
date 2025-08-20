3ab4125626177b72320ff0058d72c08654c3dba5
Yannick Van kerckvoorde <vankerckvoordeyannick@gmail.com>
2025-08-20 09:32:19 +0200

    Updated Theme

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [vue(), ui({
         ui: {
             colors: {
-                primary: 'blue',
+                primary: 'cyan',
                 neutral: 'zinc'
             }
         }
